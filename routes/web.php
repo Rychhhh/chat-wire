54d7e6a4f5f2c2c70fef5eb0631adfd1000cda68
rayhan <rayhancoding1603@gmail.com>
2022-01-25 08:46:31 +0700

    livewire : update route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 require __DIR__.'/auth.php';
 
+Route::get('/', function() {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function() {
     return view('dashboard');
 })->name('dashboard');
