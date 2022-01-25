49ae9f5ff071b11b71327b35b116565899435ae2
rayhan <rayhancoding1603@gmail.com>
2022-01-25 08:17:05 +0700

    livewire : update listpercakapan

--- app/Http/Livewire/Chat/ListPercakapan.php
+++ app/Http/Livewire/Chat/ListPercakapan.php
@@ -3,21 +3,37 @@
 namespace App\Http\Livewire\Chat;
 
 use App\Models\Percakapan;
-use App\Models\Pesan;
 use Livewire\Component;
 
 class ListPercakapan extends Component
 {
+    public $pilihanPesan;
+
+    // agar bisa akses pesan method in line 73 
+    public function mount()
+    {
+        $this->pilihanPesan = Percakapan::query()->first();
+    }
+
+    // melihat pesan setiap user 
+    public function viewPesan($pesanId)
+    {
+        // get id
+        $this->pilihanPesan = Percakapan::findOrFail($pesanId);
+
+    }
+
+
+    // view list percakapan
     public function render()
     {
         $percakapans = Percakapan::query()
-        // ->where('pengirim_id', auth()->id())
-        // ->orWhere('penerima_id', auth()->id())
         ->get();
 
 
+        // dd($percakapans);
 
-        return view('livewire.chat.list-percakapan', [
+       return view('livewire.chat.list-percakapan', [
             'percakapans' => $percakapans
         ]);
     }
