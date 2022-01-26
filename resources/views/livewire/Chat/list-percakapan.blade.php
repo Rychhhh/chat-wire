76baade81eed0945acd15b93a2e6e852f3aff063
rayhan <rayhancoding1603@gmail.com>
2022-01-26 18:34:18 +0700

    livewire : update list percakapan

--- resources/views/livewire/Chat/list-percakapan.blade.php
+++ resources/views/livewire/Chat/list-percakapan.blade.php
@@ -1,13 +1,13 @@
 <div>
 
-    <div class="container">
+    <div class="container" wire:poll>
     <h3 class=" text-center">Chat</h3>
     <div class="messaging">
           <div class="inbox_msg">
             <div class="inbox_people">
               <div class="headind_srch">
                 <div class="recent_heading">
-                  <h4>Recent</h4>
+                  <h4>Recent {{ Auth::user()->name }}</h4>
                 </div>
                 <div class="srch_bar">
                   <div class="stylish-input-group">
@@ -24,35 +24,40 @@
                   percakapan yang diambil data users saja seperti ( nama, email, dll )  
                   note: sambil liat model percakapan
                 --}}
+                
                 @foreach ($percakapans as $item)
 
-                @php
-                    // for random image
-                    $string = mt_rand(15, 20);
+                    @php
+                        // for random image
+                        $string = mt_rand(15, 20);
 
-                    $date = date(now());
+                        $date = date(now());
 
-                @endphp
-
-              {{ dd(auth()->id()) }}
+                    @endphp                 
                     
-                <div class="chat_list {{ $item->id === $pilihanPesan->id ? 'active_chat' : '' }}" style="cursor: pointer">
+                    @if (!is_null($item->pesan->first()))    
 
-                  <div class="chat_people" wire:click.prevent='viewPesan({{ $item->id }})'>
-                    <div class="chat_img"> <img src="https://randomuser.me/api/portraits/women/{{ $string }}.jpg" alt="sunil"> </div>
-                    <div class="chat_ib">
+                         <div class="chat_list {{ $item->id === $pilihanPesan->id ? 'active_chat' : '' }}" style="cursor: pointer">
 
-                    @if (!is_null($item->pesan->first()))
-                                           
-                     <h5>{{ $item->penerima->name }} <span class="chat_date">{{ $date = now() ?  'Today ' : 'Yesterday ' }}{{ $item->pesan->first()->created_at->format($date ? 'H:i' : 'd/m/Y H:i') }}</span></h5>
-                     <p>{{$item->pesan->last()->body}}</p>
+                          <div class="chat_people" wire:click.prevent='viewPesan({{ $item->id }})'>
+                            <div class="chat_img"> <img src="https://randomuser.me/api/portraits/women/{{ $string }}.jpg" alt="sunil"> </div>
+                            <div class="chat_ib">
 
-                     @endif 
+                            <h5>{{ $item->pengirim_id === auth()->id() ? $item->penerima->name : $item->pengirim->name }} 
+                              <span class="chat_date">
+                                {{ $date = now() ?  'Today ' : 'Yesterday ' }}{{ $item->pesan->first()->created_at->format($date ? 'H:i' : 'd/m/Y H:i') }}
+                              </span>
+                            </h5>
+                            <p>{{$item->pesan->last()->body}}</p>
 
-                    </div>
-                  </div>
-                </div>
-                  @endforeach
+                            </div>
+                          </div>
+                        </div>
+                    
+
+                    @endif 
+
+                @endforeach
 
                   
               </div>
@@ -62,37 +67,55 @@
                 
               <div class="msg_history">
 
-                <div class="incoming_msg">
-                  <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-
-                  @if (!is_null($pilihanPesan))
+                {{-- Jika tidak ada pesan --}}
+                @if (!is_null($pilihanPesan))
                       
-                      @foreach ($pilihanPesan->pesan as $message)
-                      <div class="received_msg">
-                        <div class="received_withd_msg">
+                  @foreach ($pilihanPesan->pesan as $message)
+
+                  {{-- Kondisi untuk penerima dan pengirim --}}
+                      @if ($message->users_id === auth()->id())
+                      <div class="outgoing_msg">
+                        <div class="sent_msg">
                           <p>{{$message->body}}</p>
-                          <span class="time_date">{{$message->created_at->format('H:i:s')}}</span></div>
+                          <span class="time_date"> {{$message->created_at->format('H:i:s')}} </span>
                         </div>
-                        @endforeach
+                      </div>
 
-                    @endif
+                      @else
+                    
+                        <div class="incoming_msg">
+                          <div class="incoming_msg_img">
+                          </div>
 
-                </div>
-                <div class="outgoing_msg">
-                  <div class="sent_msg">
-                    <p>Test which is a new approach to have all
-                      solutions</p>
-                    <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-                </div>
-                
+                              <div class="received_msg">
+                                <div class="received_withd_msg">
+                                  <p>{{$message->body}}</p>
+                                  <span class="time_date">{{$message->created_at->format('H:i:s')}}</span>
+                                </div>
+                              </div>
+
+                        </div>   
+                      @endif
+  
+                  @endforeach
+
+                @endif
 
               </div>
+
               
+                
               <div class="type_msg">
-                <div class="input_msg_write">
-                  <input type="text" class="write_msg" placeholder="Type a message" />
-                  <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                </div>
+
+                {{-- input message --}}
+                <form class="input_msg_write" autocomplete="off" wire:submit.prevent="isiChat">
+
+                  <input type="text" class="write_msg" placeholder="Type a message" wire:model.defer="body" />
+                  <button class="msg_send_btn" type="submit">
+                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                  </button>
+
+                </form>
               </div>
             </div>
           </div>
@@ -104,6 +127,6 @@
     </div>
     {{-- <h1>Test</h1> --}}
     
-    
+-    
     </div>
     
