e9960a2858918feea74d1281567e44689907d3cf
rayhan <rayhancoding1603@gmail.com>
2022-01-26 18:31:28 +0700

    livewire : add fitur send message

--- app/Http/Livewire/Chat/ListPercakapan.php
+++ app/Http/Livewire/Chat/ListPercakapan.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire\Chat;
 
 use App\Models\Percakapan;
+use App\Models\Pesan;
 use Livewire\Component;
 
 class ListPercakapan extends Component
 {
+    // variable pesan dikiri
     public $pilihanPesan;
+    
+    // variable untuk mengirim pesan di form pesan
+    public $body;
 
-    // agar bisa akses pesan method in line 73 
+    // agar bisa akses pesan  method foreach in line 73 
     public function mount()
     {
         $this->pilihanPesan = Percakapan::query()->first();
@@ -23,15 +28,30 @@ class ListPercakapan extends Component
 
     }
 
+    public function isiChat()
+    {
+        Pesan::create([
+            'percakapans_id' => $this->pilihanPesan->id,
+            'users_id' => auth()->id(),
+            'body' => $this->body
+        ]);
+
+        $this->reset('body');
+
+        return redirect('pesan');
+    }
+
 
     // view list percakapan
     public function render()
     {
         $percakapans = Percakapan::query()
+        ->where('penerima_id', auth()->id())
+        ->orWhere('pengirim_id', auth()->id())
         ->get();
 
+            
 
-        // dd($percakapans);
 
        return view('livewire.chat.list-percakapan', [
             'percakapans' => $percakapans
